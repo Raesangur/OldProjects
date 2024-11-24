31deefe3b4c2e88b6a68eb858f8f016f9eddf86e
Pascal-Emmanuel Lachance <17782420+Raesangur@users.noreply.github.com>
2024-11-24 00:50:50 -0500

    Add files via upload

---
+++ Custom System/Cursors/ChangeCursors.bat
@@ -0,0 +1,3 @@
+REG import "E:\Pascal5333\Custom System\Cursors\Registry\Cursors_%1.reg"
+powershell.exe -ExecutionPolicy remotesigned -File "E:\Pascal5333\Custom System\Cursors\UpdateCursors.ps1"
+exit
---
+++ Custom System/Cursors/UpdateCursors.ps1
@@ -0,0 +1,8 @@
+﻿$CSharpSig = @’
+[DllImport("user32.dll")]
+public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, uint pvParam, uint fWinIni);
+‘@
+
+$CursorRefresh = Add-Type -MemberDefinition $CSharpSig -Name WinAPICall -Namespace SystemParamInfo –PassThru
+
+$CursorRefresh::SystemParametersInfo(0x0057,0,$null,0)
